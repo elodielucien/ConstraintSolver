af6e073e4ce8b6e65262b0660591d902eb68ae20
elodielucien <elodielucien@gmail.com>
2019-11-19 18:40:10 +0100

    création des composants input, puzzle et output

--- solver-app/src/app/app.module.ts
+++ solver-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { PuzzleComponent } from './puzzle/puzzle.component';
+import { OutputComponent } from './output/output.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    PuzzleComponent,
+    OutputComponent
   ],
   imports: [
     BrowserModule
